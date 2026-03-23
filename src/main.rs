2ea3dea4d3f5b70af8d4946c289d972efc659190
Sagbyy <sbouhdjeur.gagny@gmail.com>
2026-03-23 18:17:27 +0100

    Enhance CLI functionality by implementing command handling in main.rs; added support for starting servers, listing capabilities, and client commands for ordering and retrieving recipes.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,32 @@ mod protocol;
 
 use clap::Parser;
 use cli::Cli;
+use cli::command::Commands;
+use cli::client::ClientCommands;
 
 fn main() {
     let cli = Cli::parse();
 
-    println!("{:?}", cli.command);
+    match cli.command {
+        Commands::Start(args) => {
+            println!("Starting server on {:?}...", args);
+        }
+        Commands::StartTui(args) => {
+            println!("Starting TUI server on {:?}...", args);
+        }
+        Commands::ListCapabilities => {
+            println!("Listing capabilities...");
+        }
+        Commands::Client(args) => match args.command {
+            ClientCommands::Order { recipe } => {
+                println!("Ordering recipe '{}' from {}...", recipe, args.peer);
+            }
+            ClientCommands::ListRecipes => {
+                println!("Listing recipes from {}...", args.peer);
+            }
+            ClientCommands::GetRecipe { recipe } => {
+                println!("Getting recipe '{}' from {}...", recipe, args.peer);
+            }
+        },
+    }
 }
